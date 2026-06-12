be6b1174c41e51e5be01c4e123aa23232ffc5b86
Jyothi <reddyjyothi056@gmail.com>
2026-06-12 11:36:39 +0530

    updated the project

--- health_monitor.py
+++ health_monitor.py
@@ -1,16 +1,55 @@
 import psutil
+import socket
+from datetime import datetime
+hostname = socket.gethostname()
+current_time = datetime.now()
 
-print("===== SERVER HEALTH REPORT =====")
 
 cpu = psutil.cpu_percent(interval=1)
 memory = psutil.virtual_memory().percent
 disk = psutil.disk_usage('/').percent
 
-print(f"CPU Usage: {cpu}%")
-print(f"Memory Usage: {memory}%")
-print(f"Disk Usage: {disk}%")
 
-if cpu < 80 and memory < 80 and disk < 80:
-    print("\nServer Status: HEALTHY")
+print("=" * 40)
+print("SERVER HEALTH REPORT")
+print("=" * 40)
+
+print(f"Hostname      : {hostname}")
+print(f"Timestamp     : {current_time}")
+
+print("\nSystem Metrics")
+print("-" * 40)
+
+print(f"CPU Usage     : {cpu}%")
+print(f"Memory Usage  : {memory}%")
+print(f"Disk Usage    : {disk}%")
+
+print("\nHealth Checks")
+print("-" * 40)
+
+warning = False
+
+if cpu > 80:
+    print("WARNING: High CPU Usage")
+    warning = True
+
+if memory > 80:
+    print("WARNING: High Memory Usage")
+    warning = True
+
+if disk > 80:
+    print("WARNING: High Disk Usage")
+    warning = True
+
+if not warning:
+    print("All system metrics are within safe limits.")
+
+print("\nOverall Status")
+print("-" * 40)
+
+if warning:
+    print("SERVER STATUS: WARNING")
 else:
-    print("\nServer Status: WARNING")
+    print("SERVER STATUS: HEALTHY")
+
+print("=" * 40)
